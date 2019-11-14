ab06041450c1ea5db60a433d86e2519c23f8c7ac
Think <zhangzichen@jinits.com>
2019-11-14 18:48:40 +0800

    创建辅助函数

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,1 +1,27 @@
 <?php
+
+if (!function_exists('is_create')) {
+
+    /**
+     * 是否为创建
+     *
+     * @return bool
+     */
+    function is_create()
+    {
+        return request()->isMethod('post');
+    }
+}
+
+if (!function_exists('is_update')) {
+
+    /**
+     * 是否为创建
+     *
+     * @return bool
+     */
+    function is_update()
+    {
+        return request()->isMethod('put');
+    }
+}
