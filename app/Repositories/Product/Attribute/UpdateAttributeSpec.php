171a13ba4143f8c08a6dd9d1c957e226d395e94f
Think <zhangzichen@jinits.com>
2019-11-20 23:00:03 +0800

    增加UpdateAttributeSpec 功能

---
+++ app/Repositories/Product/Attribute/UpdateAttributeSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories\Product\Attribute;
+
+use App\Models\Product\AttributeSpec;
+use App\Repositories\BaseRepository;
+
+class UpdateAttributeSpec extends BaseRepository
+{
+    public function rules()
+    {
+        return [
+            'id' => 'required|integer|exists:attribute_specs,id',
+            'attribute_id' => 'required|integer|exists:attributes,id',
+            'spec_name' => [is_create() ? 'unique_with:attribute_specs,attributes_id,spec_name' : 'filled','required','string','max:50'],
+        ];
+    }
+
+    public function execute(array $data) : AttributeSpec
+    {
+        $this->validate($data);
+
+        $spec_name = AttributeSpec::findOrfail($data['id']);
+        $spec_name->update([
+            'spec_name' => $data['spec_name'],
+        ]);
+        return AttributeSpec::findOrfail($data['id']);
+    }
+}
